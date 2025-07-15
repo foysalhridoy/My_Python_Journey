33dca3aefce2c540eefe09f860862b45e2dbfc2c
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-07-15 23:28:07 +0600

    Variables_DataTypes.py

---
+++ Variables_DataTypes.py
@@ -0,0 +1,24 @@
+a=complex(2,1) #print complex number
+a1=9
+print(a)
+
+b="Hridoy"
+b1=True
+print(b)
+
+print(a+a1)
+
+print("The type of a is", type(a))
+print("The type of a1 is", type(a1))
+print("The type of b is", type(b))
+print("The type of b1 is", type(b1))
+
+#list, tupple, dict
+list1 = [[8,2.3],[-4,5],["apple","banana"]]
+print(list1)
+
+tuple1 = (("parrot","crow"),("Hridoy","Proma"),("cow","chicken"))
+print(tuple1)
+
+dict1 = {"Name":"Hridoy", "Age":"23", "Education":"Bachelor"}
+print(dict1)
