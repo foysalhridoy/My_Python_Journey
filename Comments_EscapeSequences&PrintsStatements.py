8b53ff7955036c462bca40f63e220e1072caef37
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-07-15 23:27:23 +0600

    Comments_EscapeSequences&PrintsStatements.py

---
+++ Comments_EscapeSequences&PrintsStatements.py
@@ -0,0 +1,17 @@
+print("Hey,I am a good boy\nand Hridoy bro is really awsome!")
+# always respect Hridoy Bro (Commenting on python using Hash symbol)
+# Code author: Hridoy Bro 
+# print("Hello Visitors!")  Shortcut: ctrl+/
+
+'''
+Md. Ferdous Foysal Hridoy (Multiline Comments)
+'''
+
+"""
+Md. Ferdous Foysal Hridoy (Multiline Comments)
+"""
+
+print("Hridoy is a \"good boy\"") #using \" for highlighting
+
+print("Hey",6,7, sep="-", end="009\n")
+print("Hridoy")
