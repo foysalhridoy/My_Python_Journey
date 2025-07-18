c27ba5e834924367093c2e95293bec1ff4ed8102
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-07-18 23:33:01 +0600

    Strings.py

---
+++ Strings.py
@@ -0,0 +1,25 @@
+# name = "Hridoy"
+
+# friend = "Proma"
+
+# another_Friend = 'Tanvir' #string ""/'' eo use kora jay
+
+apple = 'Apple said, "I am apple" '
+
+# Three_line = """Hey
+# hello
+# bye bye"""  #prints 3 line of statement using 3 single or double quotes
+
+# print("Hello, " + friend)
+# print(apple)
+# print(Three_line)
+# print(name[0]) #order of strings, here print "H"
+# print(name[1]) #order of strings, here print "r"
+# print(name[2]) #order of strings, here print "i"
+# print(name[3]) #order of strings, here print "d"
+# print(name[4]) #order of strings, here print "o"
+# print(name[5]) #order of strings, here print "y"
+
+print("let's use a for loop") 
+for character in apple:   #looping through the strings
+    print(character)   #amra ekta string er sob gula character loop use kore ber korte parchi j konta koto number string er moddhe porche by number
