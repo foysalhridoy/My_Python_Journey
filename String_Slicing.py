e1781847670a2b55d8bccc1f5a1a2056fd3ff241
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-07-19 00:04:10 +0600

    String_Slicing.py

---
+++ String_Slicing.py
@@ -0,0 +1,17 @@
+# names = ("Hridoy,Proma")
+# print(names[0:6]) #Proma r Hridoy er moddhe Hridoy Print korar jonno eta korte hobe
+
+# print(len(names)) #string er character length calculate kore
+
+fruit = "Mango"
+MangoLen = len(fruit)
+print(MangoLen)
+print(fruit[0:4])
+print(fruit[1:4])
+print(fruit[:4]) #python auto 0 dhore ney
+print(fruit[2:4])
+print(fruit[3:4])
+print(fruit[:]) #python auto 0 to 5 dhore ney
+print(fruit[0:-3]) #0:[5-3]=2 means [0:2]
+print(fruit[-1:-3]) #-1 means [5-1]=4 and -3 means [5-3]=2 that's means [4:2] ----> etar mane eta kono kichui print korbe na faka jaiga rakhbe karon 4 theke 2 possible na as a character
+print(fruit[-3:-1]) #-3 means [5-3]=2 and -1 means [5-1]=4 that's means [2:4]
