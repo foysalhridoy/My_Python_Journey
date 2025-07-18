febac2407c591d1e810e18c0554d3e0b9333e93b
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-07-18 22:54:03 +0600

    User_input.py

---
+++ User_input.py
@@ -0,0 +1,8 @@
+# a = input("Enter your name: ")
+# print("My name is",a)
+
+x = int(input("Enter 1st num: ")) #int na dile setake python string vebe ney & output o string er motoi dekhay tai int use kora
+y = int(input("Enter 2nd num: "))
+print(x + y)
+#print(int(x) + int(y))
+
