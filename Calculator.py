9286c3aac30c0846d4d0952f655e331cf55342ba
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-07-18 22:52:47 +0600

    Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+# print(15+6) #Addition
+# print(15-6) #Substraction
+# print(15*6) #Multiplication
+# print(15/6) #Division
+# print(15//6) #(floor division) float ba doshomik songkha cole jabe just 2 print hobe
+# print(15%6) #Modulus
+# print(5**3) #5 to the power 5 to the power 5 means 5 cube
+
+#----------------------------------------------------------------------#
+
+a=10
+b=10
+
+add=a+b
+print("The Addition of", a, "and", b, "is:",add)
+
+sub=a-b
+print("The Substraction of", a, "and", b, "is:",sub)
+
+mul=a*b
+print("The Multiplication of",a,"and", b,"is:",mul)
+
+div=a//b #doshomik number bad deyar jonno // double bar deya
+print("The Division of", a,"and",b,"is:",div)
+
