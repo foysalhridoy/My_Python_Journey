2d097fda8c3c26faa1286e420126033b979d6f93
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-07-18 22:53:20 +0600

    Typecasting.py

---
+++ Typecasting.py
@@ -0,0 +1,13 @@
+# a="10"
+# b="0"
+# print(a+b) #output=100 ; addition of strings
+# print(int(a)+int(b)) #output=10 ; addition of integers
+
+# a="10" #string
+# b=10
+# print(int(a)+b) #addition of string and int
+
+#Implicit Typecasting (int + folat = float)
+a=100.9
+b=100
+print(a+b)
