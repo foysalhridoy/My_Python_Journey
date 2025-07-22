345c51d4ef7844eab22eef26127933bba85f5432
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-07-22 10:05:36 +0600

    ForLoop.py

---
+++ ForLoop.py
@@ -0,0 +1,32 @@
+name = 'Hridoy'
+for i in name:
+    print(i, end = ", ")
+    
+#----------------------------------------------------------------#    
+
+name = 'Hridoy'
+for i in name:
+    print(i)    
+    if(i == "d"):
+        print("This is special character")
+
+#----------------------------------------------------------------#
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+#---------------------------------------------------------------#
+
+for k in range(5):
+    print(k)
+    
+for k in range(1,12,2): #even numbers (1 tp 12 er moddhe)
+    print(k+1)
+    
+for k in range(1,12,3): #odd numbers (1 to 12 er moddhe)
+    print(k+1)    
+    
+    
