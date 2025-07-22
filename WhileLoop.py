a88f200f85c7e99530de073b21ed6c368da859f1
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-07-22 19:03:36 +0600

    WhileLoop.py

---
+++ WhileLoop.py
@@ -0,0 +1,32 @@
+i = 0
+while(i<=3):
+    print(i)
+    i = i+1
+print("Done with the loop")    
+
+#---------------------------------------------------------------#
+
+i = int(input("Enter any number: "))
+while(i<=38):
+    i = int(input("Enter any number: "))
+    print(i)
+    #38 porjunto print kortei thakbe, 38 er upore kono songkha dile seta r print korbe na..bolbe j done with the loop
+print("Done with the loop") 
+
+#------------------------------------------------------------------#
+
+count = 5
+while(count >= 0):
+    print(count)
+    count = count - 1 
+else:
+    print("I am inside else")   
+    
+#------------------------------------------------------------------#
+
+x = 5
+while(x > 0):
+    print(x)
+    x = x-1
+else:
+    print('counter is 0')         
